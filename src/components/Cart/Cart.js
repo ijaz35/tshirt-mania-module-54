448d2c0f99acf1bb435946d7845c94391b584965
ijaz35 <ijaz.hossain1@gmail.com>
2022-11-26 22:00:06 +0600

    conditional rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart, handlerAddToDelete }) => {
-    // console.log(cart)
+    let command;
+    if (cart.length === 0) {
+        command = <div>
+            <h5>Please Select Any Tshirt </h5>
+            <p>Your cart is empty.. </p>
+        </div>
+    } else if (cart.length === 1) {
+        command = <p>Please Add More</p>
+    } else {
+        command = <p>Thanks for shopping</p>
+    }
+
     return (
         <div>
             <h2>This is cart </h2>
+
             {
                 cart.map(cartItem => <p key={cartItem._id}>{cartItem.name}
                     <button onClick={() => handlerAddToDelete(cartItem)}>X</button></p>)
             }
+            {cart.length === 3 && <div className="orange">
+                <h5>Trigonal</h5>
+                <p>You get a t-shirt free</p>
+            </div>}
+            {cart.length === 0 || <p className="orange">YaY! you are buying</p>}
+            {command}
+            {cart.length !== 4 ? <p>Keep adding</p> : <button>Remove All</button>}
+            {cart.length === 4 && <button>Review Items</button>}
         </div>
     );
 };
